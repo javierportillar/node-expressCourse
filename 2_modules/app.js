1de3c258c2927f1f543d505b1f1cc53464952be3
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2023-12-29 11:39:57 -0500

    Different way to export a variable of an module

--- 2_modules/app.js
+++ 2_modules/app.js
@@ -1,6 +1,10 @@
 const names = require('./names-module');
 const sayHi = require('./action');
+const data = require('./alternative');
+
 console.log(names);
+console.log(data);
+
 const {Pedro, Juan, Javi} = names;
 sayHi(Juan);
 sayHi(Pedro);
