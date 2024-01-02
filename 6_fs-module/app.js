146908e5c40b54b0949b12613aeff30ccccd5a46
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-01-02 15:21:07 -0500

    Deleted probe files

--- 6_fs-module/app.js
+++ 6_fs-module/app.js
@@ -1,10 +1,8 @@
 const {readFileSync, writeFileSync} = require('fs');
 // const fs = require("fs");
 
-// const textprove= fs.readFileSync('./textt1.txt');
 const firstTxt = readFileSync('./folder/text1.txt', 'utf8');
-// const secondTxt = readFileSync('./folder/text2.txt', 'utf8');
+const secondTxt = readFileSync('./folder/text2.txt', 'utf8');
 
-// const text1= readFileSync('./textt1.txt', 'utf8');
-// console.log(text1);
-// console.log(firstTxt, secondTxt);
+writeFileSync('./folder/result-sync.txt', `Here is the result: ${firstTxt}, ${secondTxt}`, {flag: 'a'});
+console.log(firstTxt, secondTxt);
