c2947ddc86b99f38d1823bcddf0260ccd1b0790c
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-01-02 15:04:41 -0500

    Example of path module and fs module

---
+++ 6_fs-module/app.js
@@ -0,0 +1,10 @@
+const {readFileSync, writeFileSync} = require('fs');
+// const fs = require("fs");
+
+// const textprove= fs.readFileSync('./textt1.txt');
+const firstTxt = readFileSync('./folder/text1.txt', 'utf8');
+// const secondTxt = readFileSync('./folder/text2.txt', 'utf8');
+
+// const text1= readFileSync('./textt1.txt', 'utf8');
+// console.log(text1);
+// console.log(firstTxt, secondTxt);
