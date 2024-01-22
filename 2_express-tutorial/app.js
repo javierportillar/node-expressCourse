5e7abfaeb0ab53766805b795b769d6aa3738c278
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2024-01-22 15:56:56 -0500

    First set up for server

--- 2_express-tutorial/app.js
+++ 2_express-tutorial/app.js
@@ -1,10 +1,27 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  // Home Page
+  if(url==='/'){
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello, World!</h1>');
   res.end();
   // res.end('<h1>¡Hello, World!</h1>');
+  }
+  // About Page
+  else if(url==='/about'){
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>About Page</h1>');
+    res.end();
+  }
+  // 404
+  else{
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write('<h1>Page not found</h1>');
+    res.end();
+  }
 });
+
 
 server.listen(4000);
